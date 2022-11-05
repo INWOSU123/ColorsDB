fd7bbc14e1b3337db3a06b3037a16a164daae0a9
Taiwo <ttejuosho@outlook.com>
2022-11-04 19:28:01 -0500

    base code

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const methodOverride = require("method-override");
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static(__dirname + "/public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(methodOverride("_method"));
+const path = require("path");
+require("dotenv").config();
+var exphbs = require("express-handlebars");
+const hbs = exphbs.create({
+  helpers: {},
+  defaultLayout: "main",
+});
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
+require(path.join(__dirname, "./routes/color-routes.js"))(app);
+
+module.exports = app;
